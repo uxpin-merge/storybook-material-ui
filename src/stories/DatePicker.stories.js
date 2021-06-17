69bbe6bd95b9d68e9d38d73befa419d469998bdf
naohiro-t <31594089+naohiro-t@users.noreply.github.com>
2021-06-17 14:50:23 +0900

    Added DatePicker

---
+++ src/stories/DatePicker.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DatePicker } from './DatePicker';
+
+export default {
+  title: 'MaterialUI/DatePicker',
+  component: DatePicker,
+};
+
+const Template = (args) => <DatePicker {...args} />;
+
+export const Primary = Template.bind({});
