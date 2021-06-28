7cd96830463b8190fba3b82e2432806a66e4071e
naohiro-t <31594089+naohiro-t@users.noreply.github.com>
2021-06-28 11:35:45 +0900

    Added Breadcrumbs

---
+++ src/stories/Breadcrumbs.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MaterialBreadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
+
+export function Breadcrumbs({ items }) {
+  const itemsLength = items.length;
+
+  return (
+    <MaterialBreadcrumbs aria-label="breadcrumb">
+      {items.map((item, i) => {
+        if (i === itemsLength - 1) {
+          return (
+            <Typography color='textPrimary' >
+              {item}
+            </Typography>
+          )
+        } else {
+          return (
+            <Link color='inherit' href="/" onClick={handleClick} >
+              {item}
+            </Link>
+          )
+        }
+      })}
+    </MaterialBreadcrumbs>
+  );
+}
+
+Breadcrumbs.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string),
+}
+
+Breadcrumbs.defaultProps = {
+  items: ['Page 1', 'Page 2', 'Page 3'],
+}
