94dc4a952c6d06fb9624831ab135d2704b857227
naohiro-t <31594089+naohiro-t@users.noreply.github.com>
2021-06-29 11:35:04 +0900

    Added disabled and required props to textfield

--- src/stories/TextField.js
+++ src/stories/TextField.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MaterialTextField from '@material-ui/core/TextField';
 
-export const TextField = ({ label, variant, error, helperText }) => {
+export const TextField = ({ label, variant, error, helperText, disabled, required }) => {
   return (
     <form noValidate autoComplete="off">
-      <MaterialTextField id="standard-basic" label={label} variant={variant} error={error} helperText={helperText} />
+      <MaterialTextField
+        id="standard-basic"
+        label={label}
+        variant={variant}
+        error={error}
+        disabled={disabled}
+        required={required}
+        helperText={helperText} />
     </form>
   );
 };
@@ -14,12 +21,16 @@ TextField.propTypes = {
   label: PropTypes.string,
   variant: PropTypes.oneOf(['standard', 'filled', 'outlined']),
   error: PropTypes.bool,
+  disabled: PropTypes.bool,
+  required: PropTypes.bool,
   helperText: PropTypes.string,
 };
 
 TextField.defaultProps = {
   label: 'Standard',
   variant: 'standard',
+  disabled: false,
+  required: false,
   error: false,
   helperText: undefined
 };
