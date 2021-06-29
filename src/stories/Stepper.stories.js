0a7abb5c2b3f37573bde1fea36f084f22d6c89de
naohiro-t <31594089+naohiro-t@users.noreply.github.com>
2021-06-29 09:12:47 +0900

    Added Stepper

---
+++ src/stories/Stepper.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Stepper } from './Stepper';
+
+export default {
+  title: 'MaterialUI/Stepper',
+  component: Stepper,
+};
+
+const Template = (args) => <Stepper {...args} />;
+
+export const Primary = Template.bind({});
