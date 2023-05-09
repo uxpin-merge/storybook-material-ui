d65c1db90c345d15154d6992cadee1620c10b9af
Evan Trujillo <evantrujillo30@gmail.com>
2023-05-09 14:51:38 +0900

    Add mui default theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,19 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from '../src/theme.js';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+
+export const decorators = [(Story) =>
+  (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Story/>
+    </ThemeProvider>)
+]
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
