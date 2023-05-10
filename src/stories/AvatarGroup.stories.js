48b7bf90a2568b6e19ed5386f7307a12c419eebb
Evan Trujillo <evantrujillo30@gmail.com>
2023-05-10 13:12:35 +0900

    Update AvatarGroup

--- src/stories/AvatarGroup.stories.js
+++ src/stories/AvatarGroup.stories.js
@@ -7,6 +7,15 @@ export default {
   component: AvatarGroup,
 };
 
-const Template = (args) => <AvatarGroup {...args} />;
-
-export const Primary = Template.bind({});
+export const Primary = {
+  args: {
+    avatars: [{
+      image: 'https://uxpin.s3.amazonaws.com/faces/44.png',
+      name: 'Remy Sharp',
+    },{
+      image: '',
+      name: 'John Doe',
+    }],
+    spacing: 'small'
+  }
+}
