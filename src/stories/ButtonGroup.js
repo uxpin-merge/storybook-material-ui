b06e971ce1cea9fb71edab448dc66554f1d3a657
Evan Trujillo <evantrujillo30@gmail.com>
2023-05-10 13:43:43 +0900

    Update ButtonGroup

--- src/stories/ButtonGroup.js
+++ src/stories/ButtonGroup.js
@@ -1,35 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import MaterialButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@mui/material/Button';
+import MaterialButtonGroup from '@mui/material/ButtonGroup';
 
-export const ButtonGroup = ({ buttons, type, ...props }) => {
+export const ButtonGroup = ({ buttons, color, ...props }) => {
   return (
     <MaterialButtonGroup
-      color={type}
+      color={color}
       {...props}
     >
       {buttons.map((button) => (
-        <Button style={{ backgroundColor: button.backgroundColor }}>{button.label}</Button>
+        <Button>{button.label}</Button>
       ))}
     </MaterialButtonGroup>
   );
 };
 
 ButtonGroup.propTypes = {
-  buttons: PropTypes.arrayOf({ label: PropTypes.string, backgroundColor: PropTypes.string }),
-  type: PropTypes.oneOf(['primary', 'secondary', 'default']),
+  buttons: PropTypes.arrayOf(PropTypes.shape({ label: PropTypes.string })),
+  color: PropTypes.oneOf(['primary', 'secondary', 'error', 'info', 'success', 'warning']),
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   variant: PropTypes.oneOf(['text', 'outlined', 'contained']),
   disabled: PropTypes.bool,
   orientation: PropTypes.oneOf(['vertical', 'horizontal']),
-};
-
-ButtonGroup.defaultProps = {
-  buttons: [{ label: 'one' }, { label: 'two' }, {  label: 'three' }],
-  size: 'medium',
-  variant: 'contained',
-  type: 'primary',
-  disabled: false,
-  orientation: 'horizontal',
+  fullWidth: PropTypes.bool,
 };
