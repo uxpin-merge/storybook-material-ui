62f4fad7609ee773d98633b51505460e1a8cea5a
Evan Trujillo <evantrujillo30@gmail.com>
2023-05-11 11:04:48 +0900

    Update Table

--- src/stories/Table.js
+++ src/stories/Table.js
@@ -1,38 +1,17 @@
 import React from 'react';
-import MaterialTable from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import MaterialTable from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import PropTypes from "prop-types";
 
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const exampleRows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-const exampleHeaders = [
-  { name: 'Dessert (100g serving)', align: '', },
-  { name: 'Calories', align: 'right', },
-  { name: 'Fat (g)', align: 'right', },
-  { name: 'Carbs (g)', align: 'right', },
-  { name: 'Protein (g)', align: 'right', }
-]
-
-export function Table({ rows, headers }) {
+export function Table({ rows, headers, padding, size }) {
   return (
     <TableContainer component={Paper}>
-      <MaterialTable>
+      <MaterialTable size={size} padding={padding}>
         <TableHead>
           <TableRow>
             {headers.map((header) => ( <TableCell align={header.align}>{header.name}</TableCell> ))}
@@ -59,10 +38,6 @@ export function Table({ rows, headers }) {
 Table.propTypes = {
   headers: PropTypes.object,
   rows: PropTypes.object,
+  padding: PropTypes.oneOf(['none', 'normal']),
+  size: PropTypes.oneOf(['small', 'medium'])
 };
-
-Table.defaultProps = {
-  headers: exampleHeaders,
-  rows: exampleRows,
-};
-
